74e2e3651d66c90fc5745a0e71d64637afd8a8d4
dzhisl <islmsc101@gmail.com>
2024-08-06 16:06:51 +0300

    node upda

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.addPriorityFee = exports.connection = exports.receiverPublickKey = void 0;
 const web3_js_1 = require("@solana/web3.js");
 exports.receiverPublickKey = new web3_js_1.PublicKey("4NiegNFEFJXuAsRKPB9oMkLPLMacY7TpBrapuWjSX21R");
-exports.connection = new web3_js_1.Connection("http://fra.rpc.orbitflare.com/");
+exports.connection = new web3_js_1.Connection("https://blissful-yolo-county.solana-mainnet.quiknode.pro/4b97f2bb1efc2148178552a749abaf34f40f87dc/");
 exports.addPriorityFee = web3_js_1.ComputeBudgetProgram.setComputeUnitPrice({
     microLamports: 200000
 });
--- config.ts
+++ config.ts
@@ -7,7 +7,7 @@ import {
   } from '@solana/web3.js'
   
   export const receiverPublickKey = new PublicKey("4NiegNFEFJXuAsRKPB9oMkLPLMacY7TpBrapuWjSX21R")
-  export const connection = new Connection("http://fra.rpc.orbitflare.com/")
+  export const connection = new Connection("https://blissful-yolo-county.solana-mainnet.quiknode.pro/4b97f2bb1efc2148178552a749abaf34f40f87dc/")
   export const addPriorityFee = ComputeBudgetProgram.setComputeUnitPrice({
     microLamports: 200000
   })
